daefd55f8b148408308dfa6e660a5467856238c0
Anas Emad <anasemadanas1@gmail.com>
2026-05-21 11:59:08 +0300

    Update script.js

---
+++ anas/js/script.js
@@ -0,0 +1,16 @@
+```javascript
+new Typed("#typed", {
+  strings: [
+    "Backend Developer",
+    "Java | Spring Boot | REST APIs",
+    "Django | Databases | APIs",
+    "Cybersecurity Learner",
+    "Building Real World Systems"
+  ],
+
+  typeSpeed: 55,
+  backSpeed: 35,
+  backDelay: 1200,
+  loop: true
+});
+```
